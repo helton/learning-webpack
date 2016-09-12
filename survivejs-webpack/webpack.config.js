98f3679636aae924611dcc0f2f9e59c35dffefed
Helton Carlos de Souza <helton.development@gmail.com>
2016-09-12 20:52:13 -0300

    [SurviveJS - Webpack] - Setup

---
+++ survivejs-webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  // We define where the files builded will be put
+  // We using the [name] to represent the entry name
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo - Survive JS'
+    })
+  ]
+};
